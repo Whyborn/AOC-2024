701c3e3aa9c39d6f7bcbdccd98386a7713e49ead
Whyborn <lachlan.s.whyborn@gmail.com>
2024-12-02 10:32:10 +1100

    Add solution part 2

--- Day1/Day1-solution.jl
+++ Day1/Day1-solution.jl
@@ -1,5 +1,5 @@
 # Author: Lachlan Whyborn
-# Last Modified: Mon 02 Dec 2024 10:11:21 AM AEDT
+# Last Modified: Mon 02 Dec 2024 10:29:30 AM AEDT
 
 using Pkg
 Pkg.activate("../AdventOfCode")
@@ -11,5 +11,9 @@ begin
     sorted = sort.(eachslice(inp, dims=2))
     ans = sum(abs.(sorted[1] - sorted[2]))
 
-    println("Solution is $(ans)")
+    println("Solution for part 1 is $(ans)")
+
+    ans = sum([count(x -> x == y, sorted[2]) * y for y = sorted[1]])
+
+    println("Solution for part 2 is $(ans)")
 end
